53370b681d8c497812a547d9d4a1adfb26621af7
Mateus Broilo <mateus.broilo90@gmail.com>
2020-08-10 17:22:31 -0300

    Add py-code
    Add comments.py

---
+++ Scraping_Data/tripadvisor/tripadvisor/spiders/comments.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class CommentsSpider(scrapy.Spider):
+    name = 'comments'
+    allowed_domains = ['tripadvisor.com.br']
+    start_urls = ['http://tripadvisor.com.br/']
+
+    def parse(self, response):
+        pass
