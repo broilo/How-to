95d8a44824bcc71f7e4bd0a1b23e038e33ad8e8d
Mateus Broilo <mateus.broilo90@gmail.com>
2020-09-08 11:45:51 -0300

    Update server.py

--- Deploy_with_flask/server.py
+++ Deploy_with_flask/server.py
@@ -11,5 +11,28 @@ def home():
     return 'Hello World'
 
 
+@app.route('/about')
+def about():
+    return 'The About page'
+
+
+@app.route('/blog')
+def blog():
+    return '''
+    <html>
+    <head> <head> 
+    <body>
+        <h2> Welcome to this blog! <h2>
+        <p> I am Mateus, the author of this blog. <p>
+    <body>
+    <html>
+    '''
+
+
+@app.route('/blog/<string:blog_id>')
+def blogpost(blog_id):
+    return 'This is blog post number ' + blog_id
+
+
 if __name__ == '__main__':
     app.run()
